a1953efd62d3ca3d73b5fc1b464c116397ef7061
deepakchauhan33 <deep651965@gmail.com>
2026-05-02 00:10:34 +0530

    Update project page

--- src/Components/SkillCategory.jsx
+++ src/Components/SkillCategory.jsx
@@ -5,10 +5,13 @@ const SkillCategory = ({ setCategory }) => {
 
     return (
         <div className='flex flex-row lg:flex-col gap-x-4 gap-y-3 p-2.5 overflow-auto'>
-            <button className='border border-transparent text-start px-4 py-0.5 rounded-sm shadow-md hover:bg-green-300/30 hover:text-green-300 cursor-pointer' onClick={() => setCategory("all")}>All</button>
-            <button className='border border-transparent text-start px-4 py-0.5 rounded-sm shadow-md hover:bg-green-300/30 hover:text-green-300 cursor-pointer' onClick={() => setCategory("library")}>Libraries</button>
-            <button className='border border-transparent text-start px-4 py-0.5 rounded-sm shadow-md hover:bg-green-300/30 hover:text-green-300 cursor-pointer' onClick={() => setCategory("language")}>Languages</button>
-            <button className='border border-transparent text-start px-4 py-0.5 rounded-sm shadow-md hover:bg-green-300/30 hover:text-green-300 cursor-pointer' onClick={() => setCategory("tool")}>Tools</button>
+            <button className='border border-transparent text-start px-4 py-0.5 rounded-sm shadow-md transition-all  hover:bg-green-300/30 hover:text-green-300 cursor-pointer ease-in-out duration-400 ' onClick={() => setCategory("all")}>All</button>
+            <button className='border border-transparent text-start px-4 py-0.5 rounded-sm shadow-md
+            transition-all  hover:bg-green-300/30 hover:text-green-300 cursor-pointer ease-in-out duration-400' onClick={() => setCategory("library")}>Libraries</button>
+            <button className='border border-transparent text-start px-4 py-0.5 rounded-sm shadow-md
+            transition-all  hover:bg-green-300/30 hover:text-green-300 cursor-pointer ease-in-out duration-400' onClick={() => setCategory("language")}>Languages</button>
+            <button className='border border-transparent text-start px-4 py-0.5 rounded-sm shadow-md
+            transition-all  hover:bg-green-300/30 hover:text-green-300 cursor-pointer ease-in-out duration-400' onClick={() => setCategory("tool")}>Tools</button>
         </div>
     )
 }
--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -13,13 +13,18 @@ const Projects = () => {
     <section className={`flex  flex-col px-4 py-10  sm:px-6 sm:py-14  md:px-8 md:py-16  lg:px-10 lg:py-20   ${theme === "dark" ? "bg-gray-950 text-white " : "bg-white"} `}>
 
 
-      <div className='mb-8 sm:mb-11 lg:mb-14 space-y-3'>
+      <div className='mb-8 sm:mb-11 lg:mb-14 space-y-3 '>
 
-        {/* HEADING */}
-        <h2 className='text-3xl sm:text-4xl lg:text-5xl font-bold'>Projects</h2>
+        {/* HEADING WITH LINE */}
+        <div className="flex items-center">
+          <h2 className="text-xl sm:text-2xl lg:text-4xl font-bold text-gradient">
+            Projects
+          </h2>
+          <div className="flex-grow border-t ml-4"></div>
+        </div>
 
         {/* CAPTION */}
-        <p className='text-md'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa voluptates iure itaque!</p>
+        {/* <p className='text-md ml-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa voluptates iure itaque!</p> */}
       </div>
 
       <div className='border grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-start items-center gap-6 p-3'>
