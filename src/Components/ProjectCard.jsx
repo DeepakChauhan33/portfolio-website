292c7f141211641d8b76d113afd9a3de792ead0e
deepakchauhan33 <deep651965@gmail.com>
2026-05-20 11:00:17 +0530

    New style for white theme in the Card

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+// React Icons
 import { motion } from 'framer-motion'
 import { FaArrowRight } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 
+
+
+import { ThemeContext } from '../Context/ThemeProvider';
+import { text } from 'framer-motion/client';
+
 const ProjectCard = ({ project }) => {
+
+    const { theme, toggleTheme } = useContext(ThemeContext);
+
+
     return (
 
         <motion.div
@@ -11,8 +22,7 @@ const ProjectCard = ({ project }) => {
             transition={{ duration: 0.3 }}
             className='group relative w-full sm:max-w-[390px] rounded-3xl overflow-hidden 
             border border-white/10 bg-white/[0.03] backdrop-blur-xl 
-            shadow-[0_8px_30px_rgb(0,0,0,0.12)] 
-            hover:border-emerald-400/40 transition-all duration-500'
+            shadow-[0_8px_30px_rgb(0,0,0,0.12)] transition-all duration-500'
         >
 
 
@@ -27,10 +37,10 @@ const ProjectCard = ({ project }) => {
                 <img
                     src={project.image}
                     alt={project.name}
-                    className='w-full h-[240px] rounded-xl object-cover transition-transform duration-700 group-hover:scale-105'
+                    className='w-full h-[240px] rounded-lg object-cover transition-transform duration-700 group-hover:scale-105'
                 />
 
-                <div className='absolute inset-0 bg-gradient-to-t from-black/70 via-black/10 to-transparent'></div>
+                {/* <div className='absolute inset-0 bg-gradient-to-t from-black/70 via-black/10 to-transparent'></div> */}
 
             </div>
 
@@ -40,11 +50,11 @@ const ProjectCard = ({ project }) => {
 
                 {/* Title */}
                 <div className='space-y-2'>
-                    <h2 className='text-2xl font-bold tracking-tight text-white'>
+                    <h2 className={`text-2xl font-bold tracking-tight ${theme === 'dark' ? 'text-white' : 'text-black'}`}>
                         {project.name}
                     </h2>
 
-                    <p className='text-sm leading-6 text-gray-400'>
+                    <p className={`text-sm leading-6  ${theme === 'dark' ? 'text-gray-400' : 'text-gray-600'}`}>
                         {project.desc}
                     </p>
                 </div>
@@ -59,12 +69,11 @@ const ProjectCard = ({ project }) => {
 
                             <span
                                 key={index}
-                                className='px-3 py-1 text-xs rounded-full 
-                                bg-white/5 border border-white/10 
-                                text-gray-300 hover:bg-emerald-400/10 
+                                className={`px-3 py-1 text-xs rounded-full border 
                                 hover:border-emerald-400/40 
                                 hover:text-emerald-300
-                                transition-all duration-300'
+                                ${theme === "dark" ? 'bg-white/5 text-gray-300 hover:bg-emerald-400/10 border-white/10' : 'bg-gray-200/60 border-gray-400'}
+                                transition-all duration-300`}
                             >
                                 {tech.name}
                             </span>
@@ -80,7 +89,7 @@ const ProjectCard = ({ project }) => {
 
                     {/* GitHub */}
                     <a
-                        href={project.github}
+                        href={project.gitRepo}
                         target='_blank'
                         className='flex items-center gap-2 px-5 py-2 rounded-xl 
                         border border-white/10 bg-white/5 
--- src/Services/projectData.js
+++ src/Services/projectData.js
@@ -9,7 +9,7 @@ const project = [
         "name": "Store Front",
         "image": storeFrontIMG,
         "link": "https://storefront0.netlify.app/",
-        "gitRepo": "http",
+        "gitRepo": "https://github.com/DeepakChauhan33/StoreFront",
         "desc": "Created a scalable e-commerce frontend using React and Tailwind CSS with reusable components, optimized search functionality, and global data handling through Redux Store.",
         "techStack": [
             { id: 1, name: "React" },
@@ -23,7 +23,7 @@ const project = [
         "name": "TrackIt",
         "image": trackItIMG,
         "link": "https://task-manager-kohl-nine-74.vercel.app/",
-        "gitRepo": "http",
+        "gitRepo": "https://github.com/DeepakChauhan33/TrackIt",
         "desc": "Designed and developed a responsive Task Manager application using JavaScript and Tailwind CSS with optimized user interaction handling through event delegation, category filtering, and persistent storage using Local Storage.",
         "techStack": [
             { id: 1, name: "JavaScript" },
@@ -36,7 +36,7 @@ const project = [
         "name": "Tax Harvesting Tool",
         "image": taxHarvestingIMG,
         "link": "https://tax-harvesting-tool.netlify.app/",
-        "gitRepo": "http",
+        "gitRepo": "https://github.com/DeepakChauhan33/tax-harvesting-dashboard",
         "desc": "The Tax Harvesting Dashboard is an interactive frontend application built using React.js and Tailwind CSS. It is designed to help users understand and optimize their capital gains from cryptocurrency investments.",
         "techStack": [
             { id: 1, name: "React" },
